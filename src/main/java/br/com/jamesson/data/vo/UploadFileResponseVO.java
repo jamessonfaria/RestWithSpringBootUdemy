b41abddf855e305bb965cc8c570c686c8b8fc12a
Jamesson Faria <jamessonjr@gmail.com>
2022-02-23 22:10:59 -0300

    Add service for upload and download files

--- src/main/java/br/com/jamesson/data/vo/UploadFileResponseVO.java
+++ src/main/java/br/com/jamesson/data/vo/UploadFileResponseVO.java
@@ -10,6 +10,16 @@ public class UploadFileResponseVO implements Serializable {
 	private String fileDownloadUri;
 	private String fileType;
 	private long size;
+	
+	public UploadFileResponseVO() {
+	}
+	
+	public UploadFileResponseVO(String fileName, String fileDownloadUri, String fileType, long size) {
+		this.fileName = fileName;
+		this.fileDownloadUri = fileDownloadUri;
+		this.fileType = fileType;
+		this.size = size;
+	}
 
 	public String getFileName() {
 		return fileName;
