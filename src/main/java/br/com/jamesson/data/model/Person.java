8714a6bbaf78bbbfeb2031d1c04d3cf74b2144da
Jamesson Faria <jamessonjr@gmail.com>
2022-03-02 21:02:07 -0300

    Change database from mysql to postgresql

--- src/main/java/br/com/jamesson/data/model/Person.java
+++ src/main/java/br/com/jamesson/data/model/Person.java
@@ -19,7 +19,7 @@ public class Person implements Serializable {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
-	@Column(name = "firt_name", nullable = false, length = 80)
+	@Column(name = "first_name", nullable = false, length = 80)
 	private String firstName;
 
 	@Column(name = "last_name", nullable = false, length = 80)
